1dc4999cbf28bdb8afaa56626e1d243c309336d3
naveendoddi <prasadnaveen847@gmail.com>
2024-11-20 18:33:20 +0530

    pushing

--- 20 nov 24.py
+++ 20 nov 24.py
@@ -31,22 +31,34 @@
 # *
 # ----------------------------------------------------------------------------------------------------
 
-for row in range(7):
-      for col in range(7):
-            if row == col:
-                  print("* ", end='')
-            else:
-                  print(' ', end='')
-
-      for col in range(7,0,-1):
-            if row == col:
-                  print("* ", end='')
-            else:
-                  print(' ', end='')
-
-      print()
-
 # for row in range(7):
-#       for col in range(7, 0, -1):
-#             print("*", end='')
+#       for col in range(7):
+#             if row == col or row+col == 7-1:
+#                   print("*", end='')
+#             else:
+#                   print(' ', end='')
+
 #       print()
+
+# *     *
+#  *   *
+#   * *
+#    *
+#   * *
+#  *   *
+# *     *
+# ----------------------------------------------------------------------------------------------------
+import math
+rows = int(input('enter rows: '))
+colum = rows * 2 + 1
+
+for row in range(rows):
+      for col in range(colum):
+            place = math.floor(colum/(row+2))
+            # print()
+            rev = colum - place
+            if place == col or rev == col:
+                  print('*', end='')
+            else:
+                  print(" ", end="")
+      print()
