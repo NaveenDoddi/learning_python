82de2038fcc5d46c41f6a053c68feb0d07efb3d5
naveendoddi <prasadnaveen847@gmail.com>
2024-12-17 10:59:14 +0530

    while loop

---
+++ 17 dec 24.py
@@ -0,0 +1,90 @@
+# while 
+# while true is used for continous iteration 
+# or 
+# it used for a set of statements will work continoues untill unless the break statement occurs
+# while True:
+#       print('1. patment failed')
+#       print('2. profile and privacy')
+#       print('3. money tranfer')
+#       print('4. exit')
+
+#       user_input = int(input('enter your choice'))
+
+#       if user_input == 1:
+#             print('welcome patment failed')
+#       elif user_input == 2:
+#             print('welcome rofile and privacy')
+#       elif user_input == 3:
+#             print('welcome money tranfer')
+#       elif user_input == 4:
+#             print('exit')
+#             break
+# ----------------------------------------------
+# while True:
+#       lst = ['1. patment failed','2. profile and priv','3. money tranfer','4. exit']
+
+#       for x in lst:
+#             print(x)
+
+#       user_input = int(input('enter your choice'))
+
+#       if user_input == 1:
+#             print('welcome patment failed')
+#       elif user_input == 2:
+#             print('welcome rofile and privacy')
+#       elif user_input == 3:
+#             print('welcome money tranfer')
+#       elif user_input == 4:
+#             print('exit')
+#             break
+# ----------------------------------------------
+# while True:
+#       lst = ['1. patment failed','2. profile and priv','3. money tranfer','4. exit']
+
+#       user_input = int(input('enter your choice'))
+#       for x in range(len(lst)):
+#             if(x == user_input-1):
+#                   print('welcome', lst[x])
+            
+#       if(user_input == 4):
+#             break
+
+# while loop
+# while looop is used for prcise iteration here we have choice of exact stoping value
+
+# ctr = 0
+# while(ctr < 5):
+#       print(ctr, end=" ")
+#       ctr += 1
+# output: 0 1 2 3 4
+
+
+                              # ------------
+
+
+# val = 'rama'
+# i = 0
+# while i < len(val):
+#       if val[i] == 'm':
+#             break
+#       else:
+#             print(val[i])
+#       i += 1
+# -------------------------------------------
+
+# val = 'rama'
+# i = 0
+# while(val[i] != 'm'):
+#       print(val[i])
+#       i += 1
+# -------------------------------------------
+
+# print 0 , 100 with 
+# val = 0
+# while(val <= 100):
+#       print(val)
+#       val+=1
+# -------------------------------------------
+
+[print(x) for x in range(101)]
+# -------------------------------------------
