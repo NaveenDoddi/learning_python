7fd369e3c050f4d9deb7d72d196fb6911600a445
naveendoddi <prasadnaveen847@gmail.com>
2024-12-23 23:21:13 +0530

    set {}

---
+++ set.py
@@ -0,0 +1,23 @@
+# SET{}
+# A set is a collection which is unordered, unchangeable*, and unindexed.
+
+
+
+# thisset = {"apple", "banana", "cherry"}
+# print(thisset)
+
+# for i in thisset:
+#       print(i)
+
+# print("banana" in thisset)
+
+# thisset.add("orange")
+
+
+# thisset = {"apple", "banana", "cherry"}
+# mylist = ["kiwi", "orange"]
+
+# thisset.update(mylist)
+
+# print(thisset)
+# output: {'banana', 'cherry', 'apple', 'orange', 'kiwi'}
