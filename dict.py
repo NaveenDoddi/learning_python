39a1a2d682face854f8ee30d969317dfb6abe20f
naveendoddi <prasadnaveen847@gmail.com>
2024-12-23 22:54:46 +0530

    {}

---
+++ dict.py
@@ -0,0 +1,27 @@
+obj = {
+      'name' : 'naveen',
+      "age" : 21,
+      "school" : "st joseph public school",
+      "palce" : "atp"
+}
+
+# print(obj.keys())
+# print(obj.values())
+# print(len(obj.values()))
+
+# for i in obj.items():
+#       print(i[0], i[1])
+
+# for i in obj.keys():
+#       print(i)
+      
+# for i in obj.values():
+#       print(i)
+
+# obj.update({'age': 31})
+# print(obj.get('age'))
+# obj["age"] = 32
+# print(obj["age"])
+
+obj.clear()
+print(obj)
