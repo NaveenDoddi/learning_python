22a76afd96852275cebd876bbcff5ec6aa2b22f7
naveendoddi <prasadnaveen847@gmail.com>
2025-02-21 11:31:33 +0530

    pushing that words_in_array.js

--- words_in_arr.js
+++ words_in_arr.js
@@ -4,20 +4,17 @@ letters = "elpoh".split("")
 
 for(let i of arr){
       bool = true
-      dup = [...letters]
+      let dup = [...letters]
       for(let j of i){
             if(dup.includes(j)){
-                  dup.slice(1,j)
-
+                  dup.splice(dup.indexOf(j),1)
             }else{
                   bool = false
                   break
             }
-      }
-      
+      }      
       if(bool){
             console.log(i)
       }
       
 }
-
